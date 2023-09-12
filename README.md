7f8b5381d1ebb6bba74f797244a8481d988927be
Yasiru <yrangana@gmail.com>
2023-09-12 11:35:45 +1000

    'Duplication of image urls' -  issue resolved

--- README.md
+++ README.md
@@ -5,11 +5,16 @@ Fetch Turtle SAT application and scrape images
 
 ### How to use
 
-main.py is the main script to run. It will fetch image urls since '2010-01-01' and save to a csv file called image_urls.txt.
+main.py is the main script to run. It will fetch image urls since '2010-01-01' and save to a csv file called image_urls_<Event_Type>.txt.
+Event types are as follows:
+
+- 1 = Turtle Sighting
+- 2 = Nest Location
+- 5 = Nest Protected
+- 6 = Turtles Saved
 
 ### Features to be developed
 
-- manage duplicates
 - download images
 - categorise images based on event type
 
--- main.py
+++ main.py
@@ -2,55 +2,59 @@
 
 import requests
 import json
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+from datetime import datetime
 import os
 from io import BytesIO
 from PIL import Image
 
-from concurrent.futures import ThreadPoolExecutor
-from tqdm import tqdm
-from datetime import datetime
+global recordType # global variable
 
-def get_markers():
-    """Get marker ids"""
+def get_markers(type):
     # get today's date
     # send request to api that feeds map data
     # get id for each marker on map
     today = datetime.today().strftime('%Y-%m-%d')
-    url = f"https://www.turtlesat.org.au/api/Turtle/siteevent?format=json&toadAdminAssessment=true&startDateOnOrAfter=2010-01-01&startDateOnOrBefore={today}&onlyPublicData=true&siteEventTypes=1,2,5,6"
+    url = f"https://www.turtlesat.org.au/api/Turtle/siteevent?format=json&toadAdminAssessment=true&startDateOnOrAfter=2010-01-01&startDateOnOrBefore={today}&onlyPublicData=true&siteEventTypes={type}"
     res = requests.get(url)
     return [t['id'] for t in res.json()['markers']]
 
 
 def get_turtle(turtle_id):
-    """Get image url"""
     # send request to api for each individual id
     # check if image is in response
     # if yes, return url and include accessToken, else none
+    
+    type = recordType # global variable
+    
     base = 'https://www.turtlesat.org.au'
-    turtle_url = f"https://www.turtlesat.org.au/api/13/1/siteevent/{turtle_id}/content?format=json"
+    turtle_url = f"https://www.turtlesat.org.au/api/13/{type}/siteevent/{turtle_id}/content?format=json"
     turtle_data = requests.get(turtle_url).json()
     if 'markerGalleryPhoto' in turtle_data:
         token = turtle_data['markerGalleryPhoto']['accessTokenForUnapproved']
-        return base + turtle_data['markerGalleryPhoto']['imageUrl'] + f'?accessToken={token}' + f'&turtle_id={turtle_id}'
-    
+        return base + turtle_data['markerGalleryPhoto']['imageUrl'] + f'?accessToken={token}' + f'&turtle_id={turtle_id}' + f'&type={type}'
 
 
-def fetch_turtleSat():
-    """Main fetch function for turtlesat data"""
+def scrape_turtlesat(type):
+    # main scraper script
     # get ids to feed to api to get individual marker image
     # fetch images concurrently by mapping get_turtle() function to marker_ids
     # return urls that are not none
-    marker_ids = get_markers()
-    with ThreadPoolExecutor(max_workers=4) as executor:
+    marker_ids = get_markers(type)
+    with ThreadPoolExecutor(max_workers=5) as executor:
         scraped_urls = list(tqdm(executor.map(get_turtle, marker_ids), total=len(
-            marker_ids), desc='Scraping Images'))
+            marker_ids), desc=f'Scraping Images type : {type}'))
     image_urls = [url for url in scraped_urls if url]
     print(f'Images Found: {str(len(image_urls))}')
     return image_urls
 
-#main function
 if __name__ == '__main__':
-    image_urls = fetch_turtleSat()
-    with open('./image_urls.txt', 'w') as filehandle:
-        json.dump(image_urls, filehandle)
-        
+    types = [1,2,5,6]
+    for type in types:
+        recordType = type # global variable
+        print(f"Record type {type}")
+        image_urls = scrape_turtlesat(type)
+        with open(f'./image_urls_{type}.txt', 'w') as filehandle:
+            json.dump(image_urls, filehandle)
+            
